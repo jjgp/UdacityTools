512da312d90a721f18876698bdc9c61e76da73f4
Jason Prasad <jwalapr@gmail.com>
2017-05-06 20:01:51 -0400

    Writing to docx

--- transcriptor.py
+++ transcriptor.py
@@ -1,20 +1,28 @@
 import sys
 import io
 import re
+from docx import Document
 
-f = open(sys.argv[1], 'r')
-o = open('foo', 'w')
+def add_transcript_to_document(path, doc):
+    f = open(path, 'r')
+    o = io.StringIO()
 
-section = re.compile('^[0-9]+$')
-timestamp = re.compile('^[0-9:,]+\\s-->\\s[0-9:,]+$')
-whitespace = re.compile('\\s+')
+    section = re.compile('^[0-9]+$')
+    timestamp = re.compile('^[0-9:,]+\\s-->\\s[0-9:,]+$')
+    whitespace = re.compile('\\s+')
+    for line in iter(f.readline, ''):
+        if not section.match(line) and \
+                not timestamp.match(line) and \
+                not whitespace.match(line):
+                    line = line.strip()
+                    line += ' '
+                    o.write(line.decode('utf-8'))
 
-for line in iter(f.readline, ''):
-    if not section.match(line) and \
-            not timestamp.match(line) and \
-            not whitespace.match(line):
-                o.write(line)
+    f.close()
+    doc.add_paragraph(o.getvalue())
+    o.close()
 
-f.close()
-o.close()
+d = Document()
+add_transcript_to_document(sys.argv[1], d)
+d.save('foo.docx')
 
